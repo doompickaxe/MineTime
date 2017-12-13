108d23661677c7a9be2c050c7e9feb536d267942
kev <kwatzal@gmail.com>
2017-12-13 17:13:45 +0100

    added test which ensures failing

--- src/test/kotlin/minetime/controller/SignUpControllerTest.kt
+++ src/test/kotlin/minetime/controller/SignUpControllerTest.kt
@@ -9,12 +9,14 @@ import org.mockito.ArgumentMatchers
 import org.mockito.Mock
 import org.mockito.Mockito.*
 import org.springframework.http.MediaType
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 import org.springframework.test.context.web.WebAppConfiguration
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.web.servlet.view.InternalResourceViewResolver
 
 @RunWith(SpringJUnit4ClassRunner::class)
 @ContextConfiguration
@@ -28,16 +30,34 @@ class SignUpControllerTest {
 
   @Before
   fun setup() {
+    val viewResolver = InternalResourceViewResolver()
+    viewResolver.setPrefix("/templates")
+    viewResolver.setSuffix(".jade")
+
     mockMvc = MockMvcBuilders
-        .standaloneSetup(SignUpController(personRepo))
+        .standaloneSetup(SignUpController(personRepo, BCryptPasswordEncoder()))
+        .setViewResolvers(viewResolver)
         .build()
 
     `when`(personRepo.save(ArgumentMatchers.any(Person::class.java))).then { invocation -> invocation.getArgument(0) }
   }
 
   @Test
-  fun testRegistrationPost() {
+  fun registrationPostShouldFail() {
     val requestBody = "firstName=Test&lastName=User&email=ema&password=1234"
+
+    mockMvc.perform(post("/signUp")
+        .contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+        .content(requestBody))
+        .andExpect { result ->
+          result.response.forwardedUrl === "/signUp"
+          verify(personRepo, times(0)).save(ArgumentMatchers.any(Person::class.java))
+        }
+  }
+
+  @Test
+  fun registrationPost() {
+    val requestBody = "firstName=Test&lastName=User&email=test@email.com&password=1234"
 
     mockMvc.perform(post("/signUp")
         .contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE)
